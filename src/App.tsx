b0f338d8fdab4788eba10805d6eb5f325ad177fd
Jensen Omega <omega90ej@gmail.com>
2023-11-05 20:45:47 -0800

    styling tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import './App.css'
+// import './App.css'
 
 function App() {
 
   return (
     <>
       <div>
+        <div 
+        style={{fontFamily: "Montserrat, Inter, sans-serif"}}
+        >test montserrat</div>
+        <div 
+        style={{fontFamily: "Montserrat", fontWeight: "700", fontSize: "55px"}}
+        >test montserrat</div>
+        <div>test regular</div>
         This is app comp
       </div>
     </>
