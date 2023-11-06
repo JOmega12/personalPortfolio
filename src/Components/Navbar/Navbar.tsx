124f15afffd118d9e948bcba0c3793ccfc66e9c7
Jensen Omega <omega90ej@gmail.com>
2023-11-06 00:04:10 -0800

    added packages and setups for fontawesome

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,37 +1,57 @@
-
 import { useState } from "react";
 import styles from "./Navbar.module.css";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
-export const Navbar = () => {
-   const [isOpen, setIsOpen] = useState(false);
-
-   const toggleNavBar = () => {
-      setIsOpen(!isOpen);
-   }
-   
-   const list = [
-      {name: "Home"},
-      {name: "About"},
-      {name: "Project"},
-      {name: "Contact"}
-   ]
-   return(
-      <header>
-         <nav className={styles.parentComponent}>
-            <h2 className={styles.name}>Jensen Omega</h2>
-            <div className={styles.listContainer}>
-               <ul className={styles.unstyledList}>
-                  {/* <li>Home</li>
+const NavLinks = () => {
+  const list = [
+    { name: "Home" },
+    { name: "About" },
+    { name: "Project" },
+    { name: "Contact" },
+  ];
+  return (
+    <ul className={styles.unstyledList}>
+      {/* <li>Home</li>
                   <li>About</li>
                   <li>Project</li>
                   <li>Contact</li> */}
-                  {list.map((item, index) => (
-                     <li className={styles.unstyledListOfList} key={index}>{item.name}</li>
-                  ))}
-               </ul>
-            </div>
-         </nav>
-      </header>
-   )
-}
+      {list.map((item, index) => (
+        <li className={styles.unstyledListOfList} key={index}>
+          {item.name}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavBar = () => {
+    setIsOpen(!isOpen);
+  };
+
+//   might have to put in for mobile for tailwind
+
+  return (
+    <header>
+      <nav className={styles.parentComponent}>
+        <h2 className={styles.name}>Jensen Omega</h2>
+        <div className={styles.listContainer}>
+         <NavLinks />
+        </div>
+        <div className={styles.mobile}>
+            <button onClick={() => toggleNavBar()}>
+            {isOpen
+               ? <FontAwesomeIcon icon={faTimes} />
+               : <FontAwesomeIcon icon={faBars} />
+            }
+            </button>
+         </div>
+      </nav>
+    </header>
+  );
+};
