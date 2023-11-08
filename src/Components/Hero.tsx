5827200b2683f866de88e71a7afbe4dd6a823cd9
Jensen Omega <omega90ej@gmail.com>
2023-11-08 02:44:52 -0800

    desktop hero section finished

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,15 +1,15 @@
 
 import ProfilePhoto from '../assets/ProfileCopy1.png';
 import Period from '../assets/period.svg';
-import Line from '../assets/Line 1.svg'
+import Line from '../assets/Line 1.svg';
+import BackgroundCircle from '../assets/Ellipse 13.svg';
 // import styles from './Hero.module.css';
 
 export const Hero = () => {
-
+//  !need to work on circle shadow behind profile photo
    return(
-      <div className="flex flex-row md:flex-row min-[368px]:flex-col gap-10 ">
-         <div className='w-full'>
-
+      <div className="flex flex-row md:flex-row min-[368px]:flex-col gap-32">
+         <div className=''>
             <div className='flex'>
                <h2 className='text-5xl font-bold'>Hello</h2>
                <div className='ml-[2px]'>
@@ -18,22 +18,34 @@ export const Hero = () => {
                </div>
             </div>
 
-            <div className='flex flex-row w-full relative ml-24 p-10'>
-               <div className='absolute -left-40 top-5 bottom-0 mt-10'>
+            <div className='flex flex-row relative 
+            ml-24 p-4
+            '>
+               <div className='absolute -left-36 top-1 bottom-0 mt-10'>
                   <img src={Line} alt="line"
                   className=''/>
                </div>
                <h2 className='text-5xl font-medium'>Im Jensen</h2>
             </div>
             
-            <h1 className='text-[55px] font-size font-bold break-keep w-full overflow-hidden'>Software Developer</h1>
+            <h1 className='text-[55px] font-size font-bold break-keep overflow-hidden' style={{ whiteSpace: 'nowrap' }}>Software Developer</h1>
             {/* might be better to create a custom button with all the css */}
-            <button>Got A Project?</button>
-            <button>Contact</button>
+            <div className='flex gap-10 mt-10'>
+               <div className='bg-[#FF715B] px-4 py-2 text-lg'>
+                  <a href="" className='text-lg'>Got A Project?</a>
+               </div>
+               <div className='px-8 py-2 text-lg border-[#FF715B] border-2'>
+                  <a href="" className=''>Contact</a>
+               </div>
+            </div>
          </div>
-         <div className='w-full'>
-            <div className="Circle"></div>
-            <div className='w-full h-full'>
+         <div className='w-[350px] relative'>
+            <div className={`circle absolute z-0 `}>
+            {/* <div className={styles.backgroundSmoke}></div> */}
+               <img src={BackgroundCircle} alt="Profile Photo Background" 
+               className=''/>
+            </div>
+            <div className='z-10 relative bottom-14 right-2'>
                <img src={ProfilePhoto} alt="Jensen's Profile Photo" 
                className='w-full h-full'
                />
